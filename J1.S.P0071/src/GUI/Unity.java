fdc0724525e18ac311827588f56775201ca3e037
Tamnhhe <tamnhhe@gmail.com>
2023-10-17 19:51:01 +0700

    New Ass

---
+++ J1.S.P0071/src/GUI/Unity.java
@@ -0,0 +1,143 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI;
+
+import java.util.Scanner;
+import Resource.Message;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ *
+ * @author Tamnhhe
+ */
+public class Unity {
+    Scanner scanner = new Scanner(System.in);
+    public int getInt(String message, String firstError,String secondError,String thirdError, int min, int max){
+        while(true){
+            try{
+                System.out.println(message);
+                String input = scanner.nextLine().trim();
+                if(input.isEmpty()){
+                    System.out.println(Message.WRONG_INPUT);
+                }else{
+                    int number = Integer.parseInt(input);
+                    if(number < min){
+                        System.out.println(firstError);
+                    }else if(number > max){
+                        System.out.println(secondError);
+                    }else{
+                        return number;
+                    }
+                }
+            }catch(Exception e){
+                System.out.println(thirdError);
+            }
+        }
+    }
+    public double getDouble(String message, String firstError,String secondError, String thirdError,double min, double max){
+        while(true){
+            try{
+                System.out.println(message);
+                String input = scanner.nextLine().trim();
+                if(input.isEmpty()){
+                    System.out.println(Message.WRONG_INPUT);
+                }else{
+                    double number = Double.parseDouble(input);
+                    if(number < min){
+                        System.out.println(firstError);
+                    }else if(number > max){
+                        System.out.println(secondError);
+                    }else{
+                        return number;
+                    }
+                }
+            }catch(Exception e){
+                System.out.println(thirdError);
+            }
+        }
+    }
+    public float getFloat(String message, String firstError,String secondError, String thirdError,double min, double max){
+        while(true){
+            try{
+                System.out.println(message);
+                String input = scanner.nextLine().trim();
+                if(input.isEmpty()){
+                    System.out.println(Message.WRONG_INPUT);
+                }else{
+                    float number = Float.parseFloat(input);
+                    if(number < min){
+                        System.out.println(firstError);
+                    }else if(number > max){
+                        System.out.println(secondError);
+                    }else{
+                        return number;
+                    }
+                }
+            }catch(Exception e){
+                System.out.println(thirdError);
+            }
+        }
+    }
+    public String getString(String message, String error, String regex){
+        while(true){
+            System.out.println(message);
+            String input = scanner.nextLine().trim();
+            if(input.isEmpty()){
+                System.out.println(Message.WRONG_INPUT);
+            }else{
+                if(input.matches(regex)){
+                    return input;
+                }else{
+                    System.out.println(error);
+                }
+            }
+        }
+    }
+    public String getDate(){
+        while(true){
+            System.out.println(Message.INPUT_DATE);
+            String input = scanner.nextLine().trim();
+            if(input.isEmpty()){
+                System.out.println(Message.WRONG_INPUT);
+            }else{
+                if(!input.matches(Message.MATCHES)){
+                    System.out.println(Message.DATE_WRONG_FORMAT);
+                }else{
+                    SimpleDateFormat dateFormat = new SimpleDateFormat(Message.DATE_FORMAT);
+                    dateFormat.setLenient(false);
+                    try{
+                        Date currentDate = new Date();
+                        Date inputDate = dateFormat.parse(input);
+                        currentDate = dateFormat.parse(dateFormat.format(currentDate));
+                        if(inputDate.after(currentDate)){
+                            return input;
+                        }else{
+                            System.out.println(Message.DATE_CONDITIONS);
+                        }
+                        
+                    }catch(Exception e ){
+                        System.out.println(Message.DATE_EXIST);
+                    }
+                }
+            }
+        }
+    }
+    public String getRequirementNameInput(){
+        String requirementName = getString(Message.REQUIREMENT, Message.WRONG,Message.REGEX_STRING);
+        return requirementName;
+        
+    }
+    public int getTaskTypeInput(){
+        int taskType = getInt(Message.TASK_TYPE, Message.WRONG,Message.WRONG,Message.WRONG,Message.MIN_TYPE, Message.MAX_TYPE);
+        return taskType;
+    }
+    public double getFromInput(){
+        double from = getDouble(Message.ENTER_FROM, Message.WRONG, Message.WRONG,Message.WRONG,Message.MIN_VALUE,Message.MAX_VALUE);
+        return from;
+        
+    }
+}
---
+++ J1.S.P0071/src/Resource/Message.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Resource;
+
+/**
+ *
+ * @author Tamnhhe
+ */
+public class Message {
+    public static final String REGEX_STRING = "[a-z A-Z 0-9]+";
+    public static final String OPTION = "Option: ";
+    public static final String WRONG_INPUT = "Input cannot be empty !!";
+    public static final String INPUT_DATE = "Enter date: ";
+    public static final String INPUT_ID = "Enter id: ";
+    public static final String MATCHES = "\\d{1,2}[-]\\d{1,2}[-]\\d{1,4}";
+    public static final String DATE_WRONG_FORMAT = "Date must be is format dd-MM-yyyy";
+    public static final String DATE_FORMAT = "dd-MM-yyyy";
+    public static final String DATE_CONDITIONS = "Date must be greater or equal ti current date !!";
+    public static final String DATE_EXIST = "Date does not exist !!";
+    public static final String REQUIREMENT = "Requirement Name: ";
+    public static final String WRONG = "Wrong";
+    public static final String NOT_FOUND = "NOT FOUND !!";
+    public static final String DELETE_FAILED = "DELETE FAILED !!";
+    public static final String TASK_TYPE = "Task Type: ";
+    public static final String ENTER_TO = "Enter to: ";
+    public static final String ENTER_FROM = "Enter from: ";
+    public static final String ENTER_ASSIGNEE = "Enter assignee";
+    public static final String ENTER_REVIEWER = "Enter reviewer";
+    public static final String OVERLAPS = "Overlaps task !!";
+    public static final int MIN_TYPE = 1;
+    public static final int MAX_TYPE = 4;
+    public static final double GUARDANTEED_VALUE = 0.5;
+    public static final int MIN_VALUE = 8;
+    
+    public static final int MAX_VALUE = 17;
+    
+}
